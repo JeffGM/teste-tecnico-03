b1cd3d6d999d4b3db260f228ea168e219a91ead1
Jefferson <jeovaneml@live.com>
2021-01-16 11:48:49 -0300

    created car entity with basic info

---
+++ src/Models/Entity/Car.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace Models\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="car")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Car {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $carId;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $model;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $color;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $year;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $licensePlate;
+
+    public function getCarId(){
+        return $this->carId;
+    }
+
+    public function getModel(){
+        return $this->model;
+    }
+
+    public function setModel($model){
+        $this->model = $model;
+    }
+
+    public function getColor(){
+        return $this->color;
+    }
+
+    public function setColor($color){
+        $this->color = $color;
+    }
+
+    public function getYear(){
+        return $this->year;
+    }
+
+    public function setYear($year){
+        $this->year = $year;
+    }
+
+    public function getLicensePlate(){
+        return $this->licensePlate;
+    }
+
+    public function setLicensePlate($licensePlate){
+        $this->licensePlate = $licensePlate;
+    }
+}
