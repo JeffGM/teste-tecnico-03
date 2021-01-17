ae24dbe6c54728e6940348a1ee0ce35c4abae54d
Jefferson <jeovaneml@live.com>
2021-01-17 15:06:49 -0300

    added cpf validation to the request validator

--- src/Models/DataValidators/RequestValidator.php
+++ src/Models/DataValidators/RequestValidator.php
@@ -34,12 +34,39 @@ class RequestValidator {
                         $eval = filter_var($data[$property], FILTER_VALIDATE_URL);
                         break;
 
+                    case 'cpf':
+                        $eval = static::validateCPF($data[$property]);
+                        break;
+
                     default:
                         $eval = gettype($data[$property]) == $expectedType;
                 }
                 if(!$eval)
-                    throw new \InvalidArgumentException("The property ${property} must be of type ${expectedType}");
+                    throw new \InvalidArgumentException("The property ${property} must be a valid ${expectedType}");
 
         }
+    }
+
+    private static function validateCPF($cpf) {
+        $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
+
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+        return true;
     }
 }
