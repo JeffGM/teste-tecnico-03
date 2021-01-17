163263738706d76350a5923723fa7924d55813a3
Jefferson <jeovaneml@live.com>
2021-01-17 14:54:12 -0300

    created rent controller class

---
+++ src/Controllers/RentController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Controllers;
+
+
+use Interfaces\ControllerPostInterface;
+
+class RentController implements ControllerPostInterface {
+
+    public function post($request, $response, array $args)
+    {
+        // TODO: Implement post() method.
+    }
+
+    public function returnCar($request, $response, array $args)
+    {
+        // TODO: Implement returnCar() method.
+    }
+}
