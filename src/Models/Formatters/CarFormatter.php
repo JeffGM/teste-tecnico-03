b563ca8f2c94c19c5629a658eaf1dfb161151b20
Jefferson <jeovaneml@live.com>
2021-01-17 14:09:26 -0300

    crated car formatter class

---
+++ src/Models/Formatters/CarFormatter.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Models\Formatters;
+
+
+use Models\Entity\Car;
+
+class CarFormatter {
+
+    public static function getAsJSON(Car $car) {
+        return json_encode(static::getAsArray($car));
+    }
+
+    public static function getAsArray(Car $car) {
+        return [
+            "carId" => $car->getCarId(),
+            "carName"=> $car->getCarName(),
+            "carModel" => $car->getModel(),
+            "color" => $car->getModel(),
+            "year" => $car->getYear(),
+            "licensePlate" => $car->getLicensePlate(),
+            "pricePerDay" => $car->getPricePerDay(),
+            "pricePerMonth" => $car->getPricePerMonth(),
+            "isAvailable" => $car->getIsAvaliable()
+        ];
+    }
+}
